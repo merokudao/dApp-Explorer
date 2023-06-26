99f6b4ecdf396afa16f7bda1ee7332c242d12ee0
thebitparticle <sanjit.maddamsetty@bitpack.me>
2023-06-27 00:12:28 +0530

    fix: docker non-ascii support try 3

--- src/fetch/fetchAppById.ts
+++ src/fetch/fetchAppById.ts
@@ -3,7 +3,7 @@ import { ApiEndpoints } from "../api/constants";
 const MEROKU_BASE_URL = process.env.API_HOST;
 
 export async function fetchAppById(id: string) {
-	// console.log(`${MEROKU_BASE_URL}/dapp/search/${id}`);
+	console.log(`${MEROKU_BASE_URL}/dapp/search/${id}`);
 	const res = await fetch(`${MEROKU_BASE_URL}/dapp/search/${id}`, {
 		headers: {
 			apiKey: process.env.NEXT_PUBLIC_MEROKU_API_KEY || "",
@@ -11,6 +11,8 @@ export async function fetchAppById(id: string) {
 	});
 	// const response = await res.json();
 	// const apps = response.data;
+
+	console.log("res from fetchappbyid", res);
 
 	const buffer = await res.arrayBuffer(); // Get the response as an ArrayBuffer
 	const decoder = new TextDecoder("utf-8"); // Create a TextDecoder with the correct encoding
