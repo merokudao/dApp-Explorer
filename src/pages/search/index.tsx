27f1df6ebf530ad9e00ed8d44375207dcced426a
thebitparticle <sanjit.maddamsetty@bitpack.me>
2023-07-11 15:46:53 +0530

    enhance: search now includes publishes apps 2

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,31 +1,38 @@
-import { useRouter } from "next/router";
-import CategoriesList from "../categories";
 import { AppList, PageLayout } from "@/components";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Column } from "../../components/layout/flex";
 import { fetchSearchResults } from "../../fetch/fetchSearchResults";
 
-export default function SearchPage({ items }) {
+export default function SearchPage() {
 	const router = useRouter();
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		const searchWord = router.query.search;
+		if (typeof searchWord === "string") {
+			fetchSearchResults(searchWord).then((res) => {
+				setItems(res);
+			});
+		}
+	}, [router.query.search]);
 
 	let child;
 	child = <AppList data={items}></AppList>;
+
+	let emptyLoading;
+	emptyLoading = (
+		<Column className="flex items-center w-full gap-y-4 justify-center h-screen">
+			<p className="text-md text-center">loading results ...</p>
+		</Column>
+	);
+
 	return (
 		<PageLayout>
 			<h1 className="text-[24px] leading-[32px] lg:text-4xl mb-8 capitalize">
 				search results for {router.query.search}
 			</h1>
-			<>{items.length > 0 ? child : <div />}</>
+			<>{items.length > 0 ? child : emptyLoading}</>
 		</PageLayout>
 	);
-}
-
-export async function getServerSideProps(ctx) {
-	const { search } = ctx.query;
-
-	const apps = await fetchSearchResults(search);
-
-	return {
-		props: {
-			items: apps,
-		},
-	};
 }
