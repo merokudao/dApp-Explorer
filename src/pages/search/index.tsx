0e534c0e4863297c48db4c145dd4919d62c06948
thebitparticle <sanjit.maddamsetty@bitpack.me>
2023-07-11 15:38:21 +0530

    enhance: search now includes publishes apps

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,31 @@
+import { useRouter } from "next/router";
 import CategoriesList from "../categories";
+import { AppList, PageLayout } from "@/components";
+import { fetchSearchResults } from "../../fetch/fetchSearchResults";
 
-export default function SearchPage() {
-    return (
-        <>
-            <CategoriesList title={"Search Result"} />
-        </>
-    )
+export default function SearchPage({ items }) {
+	const router = useRouter();
+
+	let child;
+	child = <AppList data={items}></AppList>;
+	return (
+		<PageLayout>
+			<h1 className="text-[24px] leading-[32px] lg:text-4xl mb-8 capitalize">
+				search results for {router.query.search}
+			</h1>
+			<>{items.length > 0 ? child : <div />}</>
+		</PageLayout>
+	);
+}
+
+export async function getServerSideProps(ctx) {
+	const { search } = ctx.query;
+
+	const apps = await fetchSearchResults(search);
+
+	return {
+		props: {
+			items: apps,
+		},
+	};
 }
