2f406196d82f3b917fd9580f47258bb79d47568e
piyush-bitpack <piyush.khandelwal@bitpack.me>
2023-10-05 18:12:02 +0530

    reduced-limit

--- src/fetch/fetchApps.ts
+++ src/fetch/fetchApps.ts
@@ -4,7 +4,7 @@ const MEROKU_BASE_URL = process.env.API_HOST;
 
 export async function fetchApps() {
 	const res = await fetch(
-		`${MEROKU_BASE_URL}/${ApiEndpoints.APP_LIST}?limit=300`,
+		`${MEROKU_BASE_URL}/${ApiEndpoints.APP_LIST}?limit=100`,
 		{
 			headers: {
 				apiKey: process.env.NEXT_PUBLIC_MEROKU_API_KEY || "",
